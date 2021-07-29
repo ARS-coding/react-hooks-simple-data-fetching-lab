c42d28126e52cbb149b72f38df78ddb312bd6e23
Ali Rıza <ars.lifestyle@hotmail.com>
2021-07-29 17:02:51 +0300

    Finish all the tests.

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,21 @@
-// create your App component here
+// import { response } from 'msw/lib/types'
+import React, { useEffect, useState } from 'react'
+
+function App() {
+    
+    const [imgUrl, setImgUrl] = useState()
+
+    useEffect(() => {
+        fetch(`https://dog.ceo/api/breeds/image/random`)
+        .then(response => response.json())
+        .then(json => setImgUrl(json.message))
+    }, [])
+
+    return (
+        <div className="App">
+            {imgUrl ? <img src={imgUrl} alt="A Random Dog" /> : <p>Loading...</p>}
+        </div>
+    )
+}
+
+export default App
